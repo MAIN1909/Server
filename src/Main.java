113a26559965aedc63c0e0707c820827ea4278db
Евгений <Main1909@ex.ua>
2019-01-23 00:40:12 +0200

    mission complete

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
 import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
+    private static Set<String> names = new HashSet<>();
+
+    static {
+        names.add("Sanya");
+        names.add("Alex");
+        names.add("Dimon");
+    }
     public static void main(String[] args) {
         try(ServerSocket server = new ServerSocket(8899)){
             while(true){
                 Socket s = server.accept();
                 ObjectInputStream ois = new ObjectInputStream(
                         s.getInputStream());
-                System.out.println(ois.readObject());
+                String firstMessage = (String) ois.readObject();
+                if ("registration".equals(firstMessage)) {
+                    String name = (String) ois.readObject();
+                    names.add(name);
+                    System.out.println(name + ": " + ois.readObject());
+                } else if (names.contains(firstMessage)) {
+                    System.out.println(firstMessage + ": " + ois.readObject());
+                }
                 ois.close();
                 s.close();
             }
