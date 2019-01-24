bb2623d97230586498b039e017acb03d6e9e5fda
Zhenia <Stalkerov14091998@gmail.com>
2019-01-25 01:49:11 +0200

    added synchronization on server

--- src/Main.java
+++ src/Main.java
@@ -12,25 +12,32 @@ public class Main {
         names.add("Alex");
         names.add("Dimon");
     }
+
     public static void main(String[] args) {
-        try(ServerSocket server = new ServerSocket(8899)){
-            while(true){
-                Socket s = server.accept();
-                ObjectInputStream ois = new ObjectInputStream(
-                        s.getInputStream());
-                String firstMessage = (String) ois.readObject();
-                if ("registration".equals(firstMessage)) {
-                    String name = (String) ois.readObject();
-                    names.add(name);
-                    System.out.println(name + ": " + ois.readObject());
-                } else if (names.contains(firstMessage)) {
-                    System.out.println(firstMessage + ": " + ois.readObject());
+        new Main().server();
+    }
+
+    private void server() {
+        synchronized (this) {
+            try (ServerSocket server = new ServerSocket(8899)) {
+                while (true) {
+                    Socket s = server.accept();
+                    ObjectInputStream ois = new ObjectInputStream(
+                            s.getInputStream());
+                    String firstMessage = (String) ois.readObject();
+                    if ("registration".equals(firstMessage)) {
+                        String name = (String) ois.readObject();
+                        names.add(name);
+                        System.out.println(name + ": " + ois.readObject());
+                    } else if (names.contains(firstMessage)) {
+                        System.out.println(firstMessage + ": " + ois.readObject());
+                    }
+                    ois.close();
+                    s.close();
                 }
-                ois.close();
-                s.close();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        }catch (Exception e){
-            e.printStackTrace();
         }
     }
 }
